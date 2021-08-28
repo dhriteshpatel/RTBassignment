f3d98e17658ec84018ce5390e7919860ab7cb1f7
dhriteshpatel <33549752+dhriteshpatel@users.noreply.github.com>
2021-08-28 18:21:07 +0530

    Adding ajax

--- api.js
+++ api.js
@@ -1,18 +1,48 @@
-async function fetchAPI(){
-    var h1 = document.createElement('h1');
-    h1.innerHTML = "Fetching API...";
-    h1.style.textAlign = 'center';
-    document.body.appendChild(h1);
-    var myData = await fetch("https://jsonplaceholder.typicode.com/photos")
-                       .then((data) => data.json())
-                       .then((data) => data);
-    h1.style.display = "none";
-    myData.map((item) => {
-        var myDiv = document.createElement("div");
-        myDiv.innerHTML = `<h2>${item.albumId}</h2>
-                         <img src="${item.thumbnailUrl}">
-                         <p>${item.title}</p>`;
-        document.body.appendChild(myDiv);
-    });
+
+
+window.onload = () =>{
+var div = document.createElement('div');
+div.style.display = "flex";
+div.style.justifyContent = "center";
+document.body.appendChild(div);
+var img = document.createElement("img");
+img.id = "rotate"
+img.src = "https://www.seekpng.com/png/detail/183-1834282_neon-round-circle-rounds-yuvarlak-frame-frames-border.png";
+img.style.height = "100px";
+img.style.width = "100px";
+img.style.alignSelf = "center";
+div.appendChild(img);
+console.log(img);
+var id  = setInterval(() =>{
+    if(rotateDeg === 360){
+        rotateDeg = 0;
+    }
+    document.getElementById("rotate").style.transform = `rotate(${rotateDeg}deg)`;
+    rotateDeg += 20;
+},40)
+var data = [];
+var rotateDeg = 20;
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/photos';
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange= async(e)=>{
+    data  = await Http.responseText;
+    data = JSON.parse(data);
+    clearTimeout(id);
+    div.remove();
+    data.map((item) => {
+                var myDiv = document.createElement("div");
+                myDiv.innerHTML = `<h2>${item.albumId}</h2>
+                                 <img src="${item.thumbnailUrl}">
+                                 <p>${item.title}</p>`;
+                document.body.appendChild(myDiv);
+            });
+
+    }   
 }
-fetchAPI();
+
+
+
+
+
